17bb8252061296c17c4ce84288356b05e0d2a88c
khacsinhcs <khacsinhcs@gmail.com>
2019-01-22 06:41:23 +0700

    handle get all and misc refactor

--- app/actors/CacheBaseActor.scala
+++ app/actors/CacheBaseActor.scala
@@ -1,20 +1,23 @@
 package actors
 
 import actors.events._
-import akka.Done
 import akka.actor.Actor
-import javax.xml.crypto.dsig.keyinfo.KeyName
+import model.HostConfig.Host
 import play.api.cache.redis.CacheAsyncApi
 
 import scala.concurrent.ExecutionContext
+import scala.util.Try
 
 trait CacheBaseActor[Key, Type] extends Actor {
   val cache: CacheAsyncApi
 
   implicit val executionContext: ExecutionContext
+
   import scala.concurrent.duration._
+  import scala.util.{Success, Failure}
 
   override def receive: Receive = {
+
     case CreateEvent(t: Type) =>
       val keyName: Key = keyOf(t)
       cache.set(keyCode(keyName), t, 1000.minutes)
@@ -22,17 +25,22 @@ trait CacheBaseActor[Key, Type] extends Actor {
     case UpdateEvent(keyName: Key, data: Key) =>
       cache.get[Type](keyCode(keyName)).foreach {
         case Some(_) => cache.set(keyCode(keyName), data)
-        case None => throw new RuntimeException("Something wrong")
       }
     case DeleteEvent(keyName: Key) =>
-      cache.get[Type](keyCode(keyName)).foreach {
+      cache.get[Type](keyCode(keyName)) map {
         case Some(_) =>
           cache.remove(keyCode(keyName))
           cache.set[String](s"Type($typeName)").remove(keyCode(keyName))
-        case None => throw new RuntimeException("Something wrong")
+      } onComplete {
+        case Success(_) => sender() ! true
       }
     case GetAll =>
-      ???
+      cache.get[Set[Host]](s"Type($typeName)") map {
+        case Some(s) => s
+        case None => Set()
+      } onComplete {
+        case Success(value) => sender() ! value
+      }
   }
 
   private def keyCode(keyName: Key) = {
