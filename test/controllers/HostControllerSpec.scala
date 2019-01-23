d41fb4b8dd656a7cb1598daf331c00570ccff493
khacsinhcs <khacsinhcs@gmail.com>
2019-01-23 14:57:21 +0700

    add tests

--- test/controllers/HostControllerSpec.scala
+++ test/controllers/HostControllerSpec.scala
@@ -7,7 +7,7 @@ import play.api.test.Helpers._
 import play.api.test.{FakeRequest, Injecting}
 
 class HostControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
-  "Host Api GET" should {
+  "Retrieve host" should {
     "Get specific host with correct name " in {
       val request = FakeRequest(GET, "/api/host/DEV")
       val response = route(app, request).get
@@ -66,6 +66,18 @@ class HostControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       val request = FakeRequest(POST, "/api/host").withJsonBody(json)
       val response = route(app, request).get
       status(response) mustBe OK
+    }
+  }
+
+  "Delete host" should {
+    "success and then can't get this host" in {
+      val deleteRequest = FakeRequest(DELETE, "/api/host/DEV")
+      val deleteResponse = route(app, deleteRequest).get
+      status(deleteResponse) mustBe OK
+
+      val getRequest = FakeRequest(GET, "/api/host/DEV")
+      val getResponse = route(app, getRequest).get
+      status(getResponse) mustBe NOT_FOUND
     }
   }
 }
