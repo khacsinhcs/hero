35238712038e8bcf0d24dd8c0a62865fc76e70e8
khacsinhcs <khacsinhcs@gmail.com>
2019-02-12 22:06:18 +0700

    add implicit parametters

--- app/controllers/ClientController.scala
+++ app/controllers/ClientController.scala
@@ -3,14 +3,13 @@ package controllers
 import actors.ClientActor
 import akka.actor.{ActorRef, ActorSystem}
 import akka.util.Timeout
-import com.alab.MappableHelper
 import com.alab.conf.Type
-import com.alab.model.MapValues
+import com.alab.model.HasValues
 import com.alab.mvc.action._
-import com.alab.mvc.data.Convert
 import javax.inject.Inject
 import model.config._
 import play.api.cache.redis.CacheApi
+import play.api.libs.json.{Json, OFormat}
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext
@@ -26,20 +25,21 @@ class ClientController @Inject()(cache: CacheApi, actorSystem: ActorSystem)(impl
 
   implicit val timeout: Timeout = Timeout(5 seconds)
   implicit val kind: Type = ClientConf
+  implicit val requestFormat: OFormat[Client] = Json.format[Client]
 
   val clientActor: ActorRef = actorSystem.actorOf(ClientActor.prop(executionContext, cache))
 
   def get(name: String): Action[AnyContent] = Action.async {
     (clientActor ? Get(name)).mapTo[Option[Client]] map {
-      case Some(c) =>
-        val client = MappableHelper.mapify[Client](c)
-        Ok(Convert.toJson(MapValues(client), ClientConf))
+      case Some(client) =>
+        val clientHasValue = HasValues.from(client)
+        Ok(Json.toJson(client))
       case None => NotFound
     }
   }
 
   def create: Action[AnyContent] = (BodyAsHasValue andThen Validate) { request: RequestAsHasValue[_] =>
-    val client = request.hasValues.materialize[Client](ClientConf)
+    val client = request.hasValues.materialize[Client]
     clientActor ? CreateEvent(client)
     Ok("")
   }
