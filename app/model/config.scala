4d45d7013007a712979aa374f4efc35098358688
khacsinhcs <khacsinhcs@gmail.com>
2019-03-17 10:11:59 +0700

    add type

--- app/model/config.scala
+++ app/model/config.scala
@@ -1,20 +1,21 @@
 package model
 
-import com.alab.conf.{StringKey, StringType, Type}
+import com.alab.conf._
+import com.alab.generate.CaseClassGenerator
 import model.config.{ClientConf, HostConf}
 
 package object config {
 
   object HostConf extends Type(n = "Host", des = "Environment") {
-    val name = f("name", StringKey)
-    val gateway = f("gateway", StringType)
-    val web = f("web", StringType)
-    val mls = f("mls", StringType)
+    val name: Field[String] = f("name", StringKey)
+    val gateway: Field[String] = f("gateway", StringType)
+    val web: Field[String] = f("web", StringType)
+    val mls: Field[String] = f("mls", StringType)
   }
 
   object ClientConf extends Type(n = "Client", des = "Client configuration") {
-    val short_name = f("short_name", StringKey)
-    val api_key = f("api_key", StringType)
+    val short_name: Field[String] = f("short_name", StringKey)
+    val api_key: Field[String] = f("api_key", StringType)
   }
 
 }
@@ -23,5 +24,10 @@ object Bootstrap {
   def allTypes(): Unit = {
     HostConf
     ClientConf
+  }
+
+  def main(array: Array[String]): Unit = {
+    val all = TypeSystem.types.values map (CaseClassGenerator generate _) mkString("", "\n", "")
+    println(all)
   }
 }
